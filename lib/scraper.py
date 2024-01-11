32f2fd51105ff889cea487e82af21470f5a18624
Maddie Weber <maddieweber7@gmail.com>
2024-01-11 10:16:45 -0500

    done

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,13 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-primary')[0].contents[0].strip())
+
+courses = doc.select('.heading-25-extrabold.color-black')
+
+for course in courses:
+    print(course.contents[0].strip())
+
